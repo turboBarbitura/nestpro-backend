1aac1e82a5702e08d0ab4295b1d23a62ae344e3d
turboBarbitura <turbobarbitura@mail.ru>
2022-12-10 10:53:07 +0700

    init repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from '../user/user.module';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import configurations from '../configurations';
+import {SequelizeModule} from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ //Данный импорт нужен, чтоб использовать конфиг файл глобально.
+      isGlobal: true,
+      load: [configurations]
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('db_host'),
+        port: configService.get('db_port'),
+        username: configService.get('db_user'),
+        password: configService.get('db_password'),
+        database: configService.get('db_name'),
+        synchronize: true, //Позволит это синхронизировать.
+        autoLoadModels: true, //Автозагрузка.
+        models: [], //Модели, которые будут использовать базу данных.
+      })
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
---
+++ src/mocks/index.ts
@@ -0,0 +1,272 @@
+export const users = [
+  {
+    "_id": "638c1201b91a5c19ee6c61b9",
+    "index": 0,
+    "guid": "46d49369-6ac2-4373-9084-53b18915f024",
+    "isActive": true,
+    "balance": "$1,248.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "brown",
+    "name": "Luella Whitfield",
+    "gender": "female",
+    "company": "POOCHIES",
+    "email": "luellawhitfield@poochies.com",
+    "phone": "+1 (938) 590-3044",
+    "address": "547 Elm Avenue, Allison, Delaware, 4216",
+    "about": "Deserunt cupidatat dolore ullamco ea incididunt labore. Incididunt culpa cillum sunt incididunt est officia ad adipisicing deserunt cupidatat occaecat. Proident ad dolore exercitation dolor est cillum fugiat laboris. Do cillum id minim esse anim ea ut aliqua aliqua amet dolor laboris.\r\n",
+    "registered": "2018-08-10T09:43:29 -07:00",
+    "latitude": 61.463308,
+    "longitude": -61.294613,
+    "tags": [
+      "Lorem",
+      "Lorem",
+      "incididunt",
+      "laborum",
+      "sunt",
+      "quis",
+      "incididunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Henderson Lewis"
+      },
+      {
+        "id": 1,
+        "name": "Iva Terrell"
+      },
+      {
+        "id": 2,
+        "name": "Ortega Ball"
+      }
+    ],
+    "greeting": "Hello, Luella Whitfield! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "638c1201c883239f8549200f",
+    "index": 1,
+    "guid": "fa7cafc3-1d52-47e6-94b7-24772969b698",
+    "isActive": false,
+    "balance": "$2,424.04",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "brown",
+    "name": "Celeste House",
+    "gender": "female",
+    "company": "MEDCOM",
+    "email": "celestehouse@medcom.com",
+    "phone": "+1 (893) 511-2235",
+    "address": "805 Howard Avenue, Lopezo, Idaho, 232",
+    "about": "Consectetur minim do laboris qui enim sit excepteur. Anim et ut ad anim dolor laboris aute. Ullamco qui aliqua anim velit officia amet aliqua laborum ipsum commodo aliqua laboris pariatur. Sunt minim magna pariatur nisi id elit eu eu commodo pariatur. Eiusmod et incididunt laborum reprehenderit eu commodo fugiat veniam culpa sit est. Laborum qui ut labore laboris.\r\n",
+    "registered": "2017-05-13T07:02:37 -07:00",
+    "latitude": -2.353123,
+    "longitude": -28.527312,
+    "tags": [
+      "amet",
+      "aute",
+      "adipisicing",
+      "nostrud",
+      "amet",
+      "consectetur",
+      "amet"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Brittney Mccray"
+      },
+      {
+        "id": 1,
+        "name": "Renee Guerrero"
+      },
+      {
+        "id": 2,
+        "name": "Brenda Strong"
+      }
+    ],
+    "greeting": "Hello, Celeste House! You have 6 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "638c120131ea60f0f846e6c0",
+    "index": 2,
+    "guid": "d46cc32c-ec19-46ba-950d-568db505a77c",
+    "isActive": false,
+    "balance": "$3,770.46",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Sandoval Allison",
+    "gender": "male",
+    "company": "RECRISYS",
+    "email": "sandovalallison@recrisys.com",
+    "phone": "+1 (827) 529-3557",
+    "address": "389 Bowery Street, Boomer, American Samoa, 5855",
+    "about": "Culpa sunt exercitation consectetur Lorem minim reprehenderit enim laboris aliquip dolor fugiat et. Sunt et ut cupidatat duis qui duis voluptate tempor ea proident amet est in. Ea ullamco velit do Lorem Lorem. Laboris sint consectetur amet id. Ut nostrud exercitation dolore culpa pariatur reprehenderit laborum et qui Lorem incididunt pariatur. Reprehenderit dolore nostrud aliqua mollit do ea non eiusmod fugiat incididunt duis velit ea.\r\n",
+    "registered": "2017-04-06T03:45:48 -07:00",
+    "latitude": 5.399706,
+    "longitude": 151.673564,
+    "tags": [
+      "excepteur",
+      "commodo",
+      "ex",
+      "do",
+      "mollit",
+      "sit",
+      "aute"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Avis Williamson"
+      },
+      {
+        "id": 1,
+        "name": "Colette Hampton"
+      },
+      {
+        "id": 2,
+        "name": "Lee Tillman"
+      }
+    ],
+    "greeting": "Hello, Sandoval Allison! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "638c1201b9da74492d7efbc9",
+    "index": 3,
+    "guid": "46029e7b-cec4-45be-99e7-6e883af9356a",
+    "isActive": false,
+    "balance": "$3,538.22",
+    "picture": "http://placehold.it/32x32",
+    "age": 29,
+    "eyeColor": "green",
+    "name": "Hamilton Freeman",
+    "gender": "male",
+    "company": "PURIA",
+    "email": "hamiltonfreeman@puria.com",
+    "phone": "+1 (866) 500-3946",
+    "address": "111 Poplar Avenue, Hardyville, Nebraska, 4630",
+    "about": "Cupidatat excepteur tempor quis velit. Irure esse dolor consequat tempor exercitation mollit dolor sunt elit eu labore cupidatat. Velit culpa amet cillum reprehenderit Lorem qui veniam do ut ex irure sint esse. Minim deserunt ex consectetur sint labore quis deserunt duis incididunt veniam. Duis enim exercitation do velit. Ad velit cillum exercitation quis.\r\n",
+    "registered": "2022-10-10T12:17:52 -07:00",
+    "latitude": -62.968826,
+    "longitude": 104.241289,
+    "tags": [
+      "reprehenderit",
+      "id",
+      "pariatur",
+      "reprehenderit",
+      "velit",
+      "irure",
+      "occaecat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Stephens Bean"
+      },
+      {
+        "id": 1,
+        "name": "Bryant Barton"
+      },
+      {
+        "id": 2,
+        "name": "Dale Witt"
+      }
+    ],
+    "greeting": "Hello, Hamilton Freeman! You have 3 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "638c1201e858c4805f6b4735",
+    "index": 4,
+    "guid": "93ff44e8-b5c2-4128-b3af-ab5077687686",
+    "isActive": true,
+    "balance": "$1,896.68",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Harriet Matthews",
+    "gender": "female",
+    "company": "XANIDE",
+    "email": "harrietmatthews@xanide.com",
+    "phone": "+1 (822) 438-2196",
+    "address": "856 Montieth Street, Fidelis, Wyoming, 396",
+    "about": "Exercitation ad dolore mollit duis eu ex dolore nulla. Amet Lorem sit occaecat amet cupidatat nostrud qui fugiat cupidatat culpa commodo. Velit laborum elit officia ipsum esse do aute elit velit labore cillum.\r\n",
+    "registered": "2022-02-24T01:31:56 -07:00",
+    "latitude": -26.648119,
+    "longitude": -83.259847,
+    "tags": [
+      "officia",
+      "ex",
+      "anim",
+      "irure",
+      "enim",
+      "commodo",
+      "tempor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Maddox Lucas"
+      },
+      {
+        "id": 1,
+        "name": "Leach Patton"
+      },
+      {
+        "id": 2,
+        "name": "Beryl Pierce"
+      }
+    ],
+    "greeting": "Hello, Harriet Matthews! You have 3 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "638c1201476354a3bab3e2ff",
+    "index": 5,
+    "guid": "909fdac6-4005-4821-be07-db00a81a6217",
+    "isActive": false,
+    "balance": "$1,225.73",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": "Rene Conrad",
+    "gender": "female",
+    "company": "MANTRIX",
+    "email": "reneconrad@mantrix.com",
+    "phone": "+1 (879) 438-2579",
+    "address": "428 Batchelder Street, Salix, South Dakota, 3769",
+    "about": "Ipsum veniam duis et sit velit culpa ipsum reprehenderit quis quis est duis nostrud. Exercitation non qui nisi excepteur cillum eu cillum incididunt occaecat ex occaecat ipsum officia nostrud. Nulla laboris elit in exercitation nisi tempor cupidatat adipisicing ad proident aute nostrud sunt. Non occaecat elit sunt id do deserunt culpa velit dolor magna amet dolor et nostrud. Qui minim tempor nulla sit tempor eiusmod cillum Lorem quis et sunt exercitation irure nisi. In tempor incididunt consectetur sint et. Sunt dolore pariatur enim ullamco veniam minim dolor labore Lorem.\r\n",
+    "registered": "2020-05-18T03:38:11 -07:00",
+    "latitude": 37.609433,
+    "longitude": 155.236342,
+    "tags": [
+      "irure",
+      "Lorem",
+      "laborum",
+      "magna",
+      "incididunt",
+      "nostrud",
+      "reprehenderit"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Millie Orr"
+      },
+      {
+        "id": 1,
+        "name": "Judy Talley"
+      },
+      {
+        "id": 2,
+        "name": "Kathryn Cotton"
+      }
+    ],
+    "greeting": "Hello, Rene Conrad! You have 1 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
